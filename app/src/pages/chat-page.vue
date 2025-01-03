0d1cb45db6ec90723831daa0528b45c4fe310916
Jeff Peoples <jeff@jeffpeoples.com>
2025-01-02 22:31:04 -0700

    Updated initial prompt

--- app/src/pages/chat-page.vue
+++ app/src/pages/chat-page.vue
@@ -60,7 +60,7 @@ export default {
         {
           id: 1,
           who: "GrAIdon",
-          text: "Hello, I'm GrAIdon. I'm an AI trained on the full text of Graydon Peoples memoirs. Ask me anything about the 300 pages he wrote. For example, 'What was Graydon's proudest moment in life?'",
+          text: "Hello, I'm GrAIdon. I'm an AI trained on the full text of Graydon Peoples memoirs. Ask me anything about the 300 pages he wrote - His life, growing up, stories and vacations with his children, his career, boating, grandkids. For example, 'Tell me a funny story from Graydon's family vacations'. You can also ask me to suggest topics and questions.",
         }                                   
       ]
     };
